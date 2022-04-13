8296dcac1e0b81eaf87d6aeaeaef518273e8c4a3
jeidman <jonah.eidman@gmail.com>
2022-04-13 18:40:42 -0400

    react testing file set up

---
+++ __tests__/react.js
@@ -0,0 +1,27 @@
+//imports
+import React from 'react';
+import { Provider } from 'react-redux';
+import userEvent from '@testing-library/user-event';
+import { render, screen, waitFor } from '@testing-library/react';
+import regeneratorRuntime from 'regenerator-runtime'; //necessary?
+
+//import App, 
+
+//Testing Scope
+
+// *** FavoriteCard
+    //
+// *** Header
+    //
+// *** HomeForm
+    //
+// *** RestaurantCard
+
+
+
+
+// Option questions
+// *** How do we mock state?
+// *** How do we mock props?
+// *** How do we do TDD for refactoring? Do we need to rewrite tests to reflect how state & props are changing? I would guess yes.
+
