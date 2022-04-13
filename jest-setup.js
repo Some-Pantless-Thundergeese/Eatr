c062e92e15210d827e20593a4a5071457f0056c1
Celeste Knopf <Celeste@Celestes-Air.home>
2022-04-13 16:59:47 -0400

    backend server and router testing

--- jest-setup.js
+++ jest-setup.js
@@ -1,4 +1,5 @@
 // import regeneratorRuntime from 'regenerator-runtime';
+// import '@testing-library/jest-dom/extend-expect'
 
 module.exports = () => {
   global.testServer = require('./server/server.js');
